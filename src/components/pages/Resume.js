c91f1dcebb46931d301e57ddbde25ba6fd69739e
Arlindo Pinto <perensen@gmail.com>
2023-05-17 21:32:48 -0400

    changed app name

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,6 +1,4 @@
 import React from "react";
-import { CSSTransition } from "react-transition-group";
-import "./animations.css";
 
 // Custom CSS Style
 const styles = {
@@ -20,47 +18,39 @@ const styles = {
 
 export default function Resume() {
   return (
-    <CSSTransition
-      transitionName={{
-        enter: "enter",
-        leave: "leave",
-        appear: "appear",
-      }}
-    >
-      <div>
-        <div className="card" style={styles.cardBodyStyle}>
-          <h4 className="card-header">Proficiencies</h4>
-          <div className="card-body">
-            <ul>
-              <li>HTML</li>
-              <li>Javascript</li>
-              <li>Nodejs</li>
-              <li>CSS</li>
-              <ol>
-                <li>Bootstrap</li>
-                <li>Tailwind</li>
-                <li>Foundation CSS</li>
-              </ol>
-              <li>Backend</li>
-              <ol>
-                <li>SQL</li>
-                <li>MondoDB</li>
-                <li>Apollo Server</li>
-              </ol>
-            </ul>
-          </div>
-          <div className="card-footer">
-            <i className="bi bi-arrow-down-circle-fill"></i>
-            <a
-              style={styles.linkStyle}
-              href="./assets/resume-arlindo-pinto.pdf"
-              download
-            >
-              Download my resume
-            </a>
-          </div>
+    <div>
+      <div className="card" style={styles.cardBodyStyle}>
+        <h4 className="card-header">Proficiencies</h4>
+        <div className="card-body">
+          <ul>
+            <li>HTML</li>
+            <li>Javascript</li>
+            <li>Nodejs</li>
+            <li>CSS</li>
+            <ol>
+              <li>Bootstrap</li>
+              <li>Tailwind</li>
+              <li>Foundation CSS</li>
+            </ol>
+            <li>Backend</li>
+            <ol>
+              <li>SQL</li>
+              <li>MondoDB</li>
+              <li>Apollo Server</li>
+            </ol>
+          </ul>
+        </div>
+        <div className="card-footer">
+          <i className="bi bi-arrow-down-circle-fill"></i>
+          <a
+            style={styles.linkStyle}
+            href="./assets/resume-arlindo-pinto.pdf"
+            download
+          >
+            Download my resume
+          </a>
         </div>
       </div>
-    </CSSTransition>
+    </div>
   );
 }
